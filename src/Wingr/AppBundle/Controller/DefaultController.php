646d572151e574c0fd43c2c0412341549605caeb
Ashish Datta <ashish@setfive.com>
2013-09-01 20:20:30 -0400

    - added stripe wooo

--- src/Wingr/AppBundle/Controller/DefaultController.php
+++ src/Wingr/AppBundle/Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace Wingr\AppBundle\Controller;
 
+require_once( dirname(__FILE__) . "/../../../../bin/stripe-php/lib/Stripe.php" );
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -21,9 +23,25 @@ class DefaultController extends Controller
 	 * @Template()
 	 */
 	public function dashboardAction()
-	{
-		
+	{		
 		$form = $this->createForm(new RequestType());
+		
+		if( $this->getRequest()->isMethod("POST") ){
+			
+			$form->bind( $this->getRequest() );
+			
+			if( $form->isValid() ){
+				$headers = 'From: ' . $this->getUser()->getEmail() . "\r\n" .
+						   'Reply-To: ' . $this->getUser()->getEmail() . "\r\n";
+				
+				$formData = $form->getData();				
+				mail("support@wingr.zendesk.com", $formData["subject"], $formData["body"], $headers);
+				
+				$this->get('session')->getFlashBag()->add('notice', "Your message has been sent. We'll get back to you over email!");
+				
+				return $this->redirect( $this->generateUrl("user_dashboard") );
+			}
+		}
 		
 		return array("user" => $this->getUser(), "form" => $form->createView());
 	}	
@@ -51,7 +69,7 @@ class DefaultController extends Controller
      * @Template()
      */
     public function indexAction()
-    {
+    {    
     	
     	$user = new User();
     	$form = $this->createForm(new RegistrationType("Wingr\\AppBundle\\Entity\\User"), $user);
@@ -62,6 +80,10 @@ class DefaultController extends Controller
     		
     		if( $form->isValid() ){
     			
+    			$res = \Stripe_Customer::create( array("card" => $user->getStripeToken(), 
+    										   		   "plan" => "regular", "email" => $user->getEmail()), "sk_test_tIVB0G66iuZu2kt4pZt4IHTc");    			
+    			
+    			$user->setStripeToken( $res["id"] );    			
     			$this->getDoctrine()->getManager()->persist( $user );
     			$this->getDoctrine()->getManager()->flush();
     			
