8d5eb7976efc3e6604529ce0e36948353cf03ee9
Ashish Datta <ashish@setfive.com>
2013-09-17 10:58:56 -0400

    - added the built CSS - modified the creative serve script to track clicks

--- web/serve.php
+++ web/serve.php
@@ -8,13 +8,13 @@ if( array_key_exists("tag", $_REQUEST) || array_key_exists("path", $_REQUEST) ){
 	
 	?>
 			
-document.write("<a target='_blank' href='https://wingr.me/?utm_source=fifty&utm_medium=banner&utm_content=<?php echo $path?>&utm_campaign=POF'><img src='http://wingr.me/creatives/<?php echo $path?>' /></a>");
+document.write("<a target='_blank' href='http://www.wingr.me/serve.php?id=$UID&imp_type=c&served=<?php echo $path?>&r=" + (Math.random() * 1e17) + "&url=" + window.location.href + "'><img src='http://wingr.me/creatives/<?php echo $path?>' /></a>");
 
 (function() {
   var d=document,h=d.getElementsByTagName('head')[0],s=d.createElement('script'); 
   s.type='text/javascript'; 
   s.async=true; 
-  s.src='https://ib.adnxs.com/getuid?http://www.wingr.me/serve.php?id=$UID&served=<?php echo $path?>&r=' + (Math.random() * 1e17) + '&url=' + window.location.href;  
+  s.src='https://ib.adnxs.com/getuid?http://www.wingr.me/serve.php?id=$UID&imp_type=v&served=<?php echo $path?>&r=' + (Math.random() * 1e17) + '&url=' + window.location.href;  
   h.appendChild(s); 
 })();
 
@@ -22,10 +22,24 @@ document.write("<a target='_blank' href='https://wingr.me/?utm_source=fifty&utm_
 <?php 
 }else{
 
+	if( $_REQUEST["imp_type"] == "v" ){
+		setcookie("appnexus", $_REQUEST["id"]);
+	}
+
+	if( $_REQUEST["imp_type"] == "c" ){
+		$_REQUEST["id"] = $_COOKIE["appnexus"];			
+	}
+
 	$pdo = new PDO("mysql:dbname=wingr;host=localhost", "wingr", "wingr");
-	$sth = $pdo->prepare("INSERT INTO impression (ip, appnexus, user_agent, url, tag) VALUES (:ip, :appnexus, :user_agent, :url, :tag)");
-	$sth->execute( array("ip" => $_SERVER["REMOTE_ADDR"], "appnexus" => $_REQUEST["id"], "tag" => $_REQUEST["served"],
-						 "user_agent" => $_SERVER["HTTP_USER_AGENT"], "url" => $_REQUEST["url"]) );
+	$sth = $pdo->prepare("INSERT INTO impression (ip, appnexus, user_agent, url, tag, imp_type) VALUES (:ip, :appnexus, :user_agent, :url, :tag, :imp_type)");
+	$sth->execute( array("ip" => $_SERVER["REMOTE_ADDR"], "appnexus" => $_REQUEST["id"], 
+						 "tag" => $_REQUEST["served"], "imp_type" => $_REQUEST["imp_type"], 
+					     "user_agent" => $_SERVER["HTTP_USER_AGENT"], "url" => $_REQUEST["url"]) );
+	
+	if( $_REQUEST["imp_type"] == "c" ){
+		header("Location: https://wingr.me/?utm_source=fifty&utm_medium=banner&utm_content=" . $_REQUEST["served"] . "&utm_campaign=POF");
+		die();
+	}
 	
 	echo "true;";
 	die();	
